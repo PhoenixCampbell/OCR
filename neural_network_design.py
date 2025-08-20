1a40d0bf2b638fc3b6f892473e80101772b65e56
PhoenixCampbell <phjcampbell18@gmail.com>
2025-08-20 13:42:38 -0500

    correcting code in range

--- neural_network_design.py
+++ neural_network_design.py
@@ -1,5 +1,5 @@
 #trying various number of hidden nodes and see what works best
-for i in xrange(x, 50, 5):
+for i in xrange(5, 50, 5):
     nn = OCRNeuralNetwork(i, data_matrix, data_labels, train_indices, False)
     performance = str(test(data_matrix, data_labels, test_indices, nn))
     print ("{i} Hidden Nodes: {val}").format(i=i, val=performance)
