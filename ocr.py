52334879100bcdfdfdac35e9363501eb63493e67
PhoenixCampbell <phjcampbell18@gmail.com>
2025-08-20 14:37:50 -0500

    testing and predicting added to ocr.py

--- ocr.py
+++ ocr.py
@@ -13,7 +13,7 @@ def _sigmoid_scaler(self, z):
     sum1 = y1 + np.mat(self.input_layer_bias)#add bias
     y1 = self.sigmoid(sum1)
 
-    y2 = np.dot(np.array(self.thea2), y1)
+    y2 = np.dot(np.array(self.theta2), y1)
     y2 = np.add(y2, self.hidden_layer_bias)#bias addition
     y2 = self.sigmoid(y2)
 
@@ -27,4 +27,14 @@ def _sigmoid_scaler(self, z):
     self.hidden_layer_bias +=self.LEARNING_RATE * output_errors
     self.input_layer_bias += self.LEARNING_RATE * hidden_errors
     
-    
+def predict(self, test):
+    y1 = np.dot(np.mat(self.theta1), np.mat(test).T)
+    y1 = y1 + np.mat(self.input_layer_bias)#add bias
+    y1 = self.sigmoid(y1)
+
+    y2 = np.dot(np.array(self.theta2), y1)
+    y2 = np.add(y2,self.hidden_layer_bias) #add bias
+    y2 = self.sigmoid(y2)
+
+    results = y2.T.tolist()[0]
+    return results.index(max(results))
